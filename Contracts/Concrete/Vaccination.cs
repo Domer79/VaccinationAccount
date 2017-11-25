387f0611016b1732891eeab13450bc8c7a3b2d64
dgaripov@it.ru <dgaripov@it.ru>
2017-11-25 15:25:44 +0500

    first files commit

---
+++ Contracts/Concrete/Vaccination.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Concrete
+{
+    public class Vaccination
+    {
+        public string Preparation { get; set; }
+        public string Approval { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    public class Patient
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Gender { get; set; }
+        public string Snils { get; set; }
+    }
+}
