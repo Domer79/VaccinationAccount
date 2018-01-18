db6a9f6512a5f3b5dd0d026f6d11e9fe24bbf355
dgaripov@it.ru <dgaripov@it.ru>
2018-01-18 17:03:07 +0500

    git test and !configs

---
+++ GitTestApi/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Octokit;
+
+namespace GitTestApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetUsers();
+            Console.ReadLine();
+        }
+
+        static async void GetUsers()
+        {
+            try
+            {
+                var connection = new Connection();
+                var github = new GitHubClient(new ProductHeaderValue("VaccinationAccount"));
+                var users = await github.User.Current();
+//                var user = await github.User.Get("Domer79");
+                    //.ContinueWith(task => { Console.WriteLine(task.Result); });
+                Console.WriteLine(users[0].Login + " folks love the half ogre!");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
